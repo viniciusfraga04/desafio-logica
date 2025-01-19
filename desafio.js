861338cd2cb029dbea68d88650bec5c61cf6ea45
vinicius <vinicius.ferreira.9192@gmail.com>
2025-01-19 20:51:58 -0300

    iniciando desafio

--- desafio.js
+++ desafio.js
@@ -1,2 +1,27 @@
-var dinheiro = '1000'
-console.log(dinheiro)
+var nome = 'Vinicius'
+var xp = 2000
+
+if (xp <= 1000) {
+    console.log("O Heroi de nome", nome + "está no nivel de Ferro")
+}
+else if (xp > 1000 && xp <= 2000){
+    console.log("O Heroi de nome", nome + "está no nivel de Bronze")
+}
+else if (xp > 2000 && xp <= 5000){
+    console.log("O Heroi de nome", nome + "está no nivel de Prata")
+}
+else if (xp > 5000 && xp <= 7000){
+    console.log("O Heroi de nome", nome + "está no nivel de Ouro")
+}
+else if (xp > 7000 && xp <= 8000){
+    console.log("O Heroi de nome", nome + "está no nivel de Platina")
+}
+else if (xp > 8000 && xp <= 9000){
+    console.log("O Heroi de nome", nome + "está no nivel de Ascendente")
+}
+else if (xp > 9000 && xp <= 10000){
+    console.log("O Heroi de nome", nome + "está no nivel de Imortal")
+}
+else if (xp >= 10001){
+    console.log("O Heroi de nome", nome + "está no nivel de Radiante")
+}
